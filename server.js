c4e19c953e7a59ab937b572611047007e88bacfa
ChoongwonLee <bigstep810@gmail.com>
2020-02-10 00:01:12 -0500

    Rmoved initial route comment

--- server.js
+++ server.js
@@ -4,22 +4,18 @@ const path = require('path');
 
 const app = express();
 
-// Conect Database
+// Connect Database
 connectDB();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
-
-// app.get('/', (req, res) =>
-//   res.json({ msg: 'Welcome to the ContactKeeper API...' })
-// );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-// Server static assets in production
+// Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
@@ -31,4 +27,4 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is up on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
