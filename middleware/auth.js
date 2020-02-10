9f44e5bd1d071bd54f293f26ddf11604910dcc20
ChoongwonLee <bigstep810@gmail.com>
2020-02-10 16:08:09 -0500

    created dev.env & test.env files

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
   }
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, config.get(process.env.JWTSECRET));
 
     req.user = decoded.user;
     next();
